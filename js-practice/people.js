7e911ddb59fc17197e16026d9ee5a7c67ecf4514
Seth Williams <sethpw@uw.edu>
2019-05-20 17:55:20 -0700

    Created data in a people.js file and set up a table to import that data into my webpage

---
+++ js-practice/people.js
@@ -0,0 +1,23 @@
+var people = [
+    {
+        fname: "Thomas",
+        lname: "Skellington",
+        age: 30,
+        email: "tomcat@plantlover.net",
+        description: "..."
+    },
+    {
+        fname: "Mary",
+        lname: "Pantry",
+        age: 32,
+        email: "Marygotscary@pants.org",
+        description: "...",
+    },
+    {
+        fname: "Stuart",
+        lname: "Pruart",
+        age: 26,
+        email: "Stuthepru@callme.gov",
+        description: "...",
+    },
+];
