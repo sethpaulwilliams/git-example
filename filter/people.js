bb7d0a06bb3df6c30a4eb44aa24503b34d0fe961
Seth Williams <sethpw@uw.edu>
2019-05-23 12:05:43 -0700

    Played around with dynamic filtering

---
+++ filter/people.js
@@ -0,0 +1,14 @@
+const people = [
+    {
+        "name": "Alfreds Futterkiste"
+        "country": "Germany"
+    }
+    {
+        "name": "Berglunds snabbkop"
+        "country": "Sweden"
+    }
+    {
+        "name": "Island Trading"
+        "country": "UK"
+    }
+]
